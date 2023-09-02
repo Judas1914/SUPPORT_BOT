c51e2bd0adf6a9231906bbe68147771c3065e814
Judas1914 <gleb1234abcd@gmail.com>
2023-09-02 22:51:43 +0300

    Changes to be committed: 	new file:   .gitignore 	new file:   main.py 	new file:   poetry.lock 	new file:   pyproject.toml 	new file:   settings/__init__.py 	new file:   settings/config.py 	new file:   tools/__init__.py 	new file:   tools/db.py 	new file:   tools/message.py

---
+++ settings/config.py
@@ -0,0 +1,28 @@
+# Для бота
+from aiogram import *
+from aiogram.dispatcher import Dispatcher
+from aiogram import Dispatcher, types
+from aiogram.dispatcher.filters import Text
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+import asyncio
+
+
+# Стандарт
+import re
+import configparser
+import time
+
+import json
+import os
+
+settings_file = "settings"
+config = configparser.ConfigParser()
+config.read('settings/settings.ini')
+
+def config_update():
+    with open(settings_file, 'w') as fl:
+        config.write(fl)
+    config.read(settings_file)
