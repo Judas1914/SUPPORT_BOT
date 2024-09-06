0cf731c547c8b1718288db030f1fc781b2c0cec5
Judas1914 <gleb1234abcd@gmail.com>
2024-09-06 16:49:45 +0300

    Changes to be committed: 	modified:   tools/db.py 	deleted:    tools/keybords.py 	modified:   tools/message.py

--- tools/db.py
+++ tools/db.py
@@ -1,98 +1,101 @@
-from settings import *
+# from settings import *
 
-class Client_DB:
-    def __init__(self):
-        self.__database: dict = self.__load()
+# class Client_DB:
+#     def __init__(self):
+#         self.__database: dict = self.__load()
 
-    def __load(self):
-        if os.path.exists(config["DB"]["client"]): # path.exists- проверят существует ли указанный файл в папке True - если файл есть \ False если он отсутсвует
-            with open(config["DB"]["client"], mode = "r", encoding="utf-8") as fl:
-                data = json.load(fl)
-            return(data)
-        else:
-            data = {}
-            with open(config["DB"]["client"], mode = "w", encoding="utf-8") as fl:
-                json.dump(data, fl, ensure_ascii=False)
-            return(data)
-
-
-    def __update(self):
-        """
-        Обновление базы данных в папке data
-        """
-        with open(config["DB"]["client"], mode = "w", encoding="utf-8") as fl:
-            json.dump(self.__database, fl, ensure_ascii=False)
-            
-    def get_data(self):
-        return self.__database
-
-    def add(self, id: str, message: str):
-
-        if str(id) not in list(self.__database.keys()):
-
-            """Добавление нового пользователя
-
-            Args:
-                id (str): ид пользователя
-                message (str): первое сообщение пользователя
-            """
-            self.__database[str(id)] = {
-                "buzy": False,
-                "message": [message] # закидывает полученное сообщение в тип данных списка
-            }
-            self.__update()
-        else:
-            """Дополнение сообщений от существуещего пользователя в БД. Если саппорт ещё не ответил. buzy == False
-
-            Args:
-                id (): ид пользователя
-                message (str): новое сообщение пользователя
-            """
-            self.__database[str(id)]["message"].append(message)
-            self.__update()
-
-    def remove(self, id):
-        """Удаление клиента по id из базы вопросов
-
-        Args:
-            id (_type_): ид клиента вопрос которого будет решен
-        """
-        self.__database.pop(str(id))
-        self.__update()
+#     def __load(self):
+#         if os.path.exists(config["DB"]["client"]): # path.exists- проверят существует ли указанный файл в папке True - если файл есть \ False если он отсутсвует
+#             with open(config["DB"]["client"], mode = "r", encoding="utf-8") as fl:
+#                 data = json.load(fl)
+#             return(data)
+#         else:
+#             data = {}
+#             with open(config["DB"]["client"], mode = "w", encoding="utf-8") as fl:
+#                 json.dump(data, fl, ensure_ascii=False)
+#             return(data)
 
 
-class Supports_DB:
-    def __init__(self):
-        self.__database = self.__load()
+#     def __update(self):
+#         """
+#         Обновление базы данных в папке data
+#         """
+#         with open(config["DB"]["client"], mode = "w", encoding="utf-8") as fl:
+#             json.dump(self.__database, fl, ensure_ascii=False)
 
-    def __load(self):
-        if os.path.exists(config["DB"]["support"]):
-            with open(config["DB"]["support"], mode = "r", encoding="utf-8") as fl:
-                data = json.load(fl)
-            return(data)
-        else:
-            data = {}
-            with open(config["DB"]["support"], mode = "w", encoding="utf-8") as fl:
-                json.dump(data, fl, ensure_ascii=False)
-            return(data)
+#     def get_data(self):
+#         return self.__database
 
-    def __update(self):
-        """
-        Обновление базы данных в папке data
-        """
-        with open(config["DB"]["support"], mode = "w", encoding="utf-8") as fl:
-            json.dump(self.__database, fl, ensure_ascii=False)
+#     def add(self, id: str, message: str):
+
+#         if str(id) not in list(self.__database.keys()):
+
+#             """Добавление нового пользователя
+
+#             Args:
+#                 id (str): ид пользователя
+#                 message (str): первое сообщение пользователя
+#             """
+#             self.__database[str(id)] = {
+#                 "buzy": False,
+#                 "message": [message] # закидывает полученное сообщение в тип данных списка
+#             }
+#             self.__update()
+#         else:
+#             """Дополнение сообщений от существуещего пользователя в БД. Если саппорт ещё не ответил. buzy == False
+
+#             Args:
+#                 id (): ид пользователя
+#                 message (str): новое сообщение пользователя
+#             """
+#             self.__database[str(id)]["message"].append(message)
+#             self.__update()
+
+#     def remove(self, id):
+#         """Удаление клиента по id из базы вопросов
+
+#         Args:
+#             id (_type_): ид клиента вопрос которого будет решен
+#         """
+#         self.__database.pop(str(id))
+#         self.__update()
 
 
-    def buzy(self, buzy: str, id: str):
-        """Смена "занятости"
+# class Supports_DB:
+#     def __init__(self):
+#         self.__database = self.__load()
 
-        Args:
-            buzy (bool): True or False
-            id (str): поддержки
-        """
-        self.__database["telegram_id"] = {
-            "buzy": buzy,
-            "client_id": id
-        }
-        self.__update()
+#     def __load(self):
+#         if os.path.exists(config["DB"]["support"]):
+#             with open(config["DB"]["support"], mode = "r", encoding="utf-8") as fl:
+#                 data = json.load(fl)
+#             return(data)
+#         else:
+#             data = {}
+#             with open(config["DB"]["support"], mode = "w", encoding="utf-8") as fl:
+#                 json.dump(data, fl, ensure_ascii=False)
+#             return(data)
+
+#     def get_data(self):
+#         return self.__database
+
+#     def __update(self):
+#         """
+#         Обновление базы данных в папке data
+#         """
+#         with open(config["DB"]["support"], mode = "w", encoding="utf-8") as fl:
+#             json.dump(self.__database, fl, ensure_ascii=False)
+
+
+#     def buzy(self, buzy: str, id: str):
+#         """Смена "занятости"
+
+#         Args:
+#             buzy (bool): True or False
+#             id (str): поддержки
+#         """
+#         self.__database["telegram_id"] = {
+#             "buzy": buzy,
+#             "client_id": id
+#         }
+#         self.__update()
--- tools/message.py
+++ tools/message.py
@@ -1,136 +1,142 @@
 from settings import *
 from .db import *
-from tools.keybords import *
 
+# support_data = Supports_DB()
+# sup_id_mas = support_data.get_data()
 
-support_data = Client_DB()
-sup_id_mas = support_data.get_data()
-
-sup_id = sup_id_mas["telegram_id"]["client_id"]
-client_id = ""
-msg_id_client = []
-msg_id_support = []
+# sup_id = sup_id_mas["telegram_id"]["client_id"]
+# client_id = ""
+# msg_id_client = []
+# msg_id_support = []
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
 
-    try:
-        with open(config['DB']['support'], mode = "r", encoding="utf-8") as fl:
-            sup_id_mas = json.load(fl)
-    except:
-        logging.error(traceback.format_exc())
+    await bot.send_message(message.chat.id, "Бот временно не работает")
 
-    await bot.send_message(message.chat.id,
-                "⚠️Сразу пишите свойю 👉ПОЧТУ (с которой была покупка)\n"
-                "👉ЛОГИН аккаунта с которым проблема⚠️\n"
-                "И сразу ваш вопрос, если не напишете, то вам и не ответят❗️")
+    # try:
+    #     with open(config['DB']['support'], mode = "r", encoding="utf-8") as fl:
+    #         sup_id_mas = json.load(fl)
+    # except:
+    #     logging.error(traceback.format_exc())
 
-    if sup_id_mas["telegram_id"]["buzy"] == "false":
-        msg_id = str(message.chat.id)
+    # await bot.send_message(message.chat.id,
+    #             "⚠️Сразу пишите свойю 👉ПОЧТУ (с которой была покупка)\n"
+    #             "👉ЛОГИН аккаунта с которым проблема⚠️\n"
+    #             "И сразу ваш вопрос, если не напишете, то вам и не ответят❗️")
 
-        await bot.send_message(sup_id,
-                        "Вам пишет пользователь"
-                        "\nName: " + str(message.chat.first_name) +
-                        "\nid: " + str(message.chat.id)
-                        ,reply_markup=Start_KYB(msg))
-    else:
-        time.sleep(300)
+    # if sup_id_mas["telegram_id"]["buzy"] == "false":
+    #     msg_id = str(message.chat.id)
 
-        start_chating = types.InlineKeyboardMarkup()
-        start_sup = types.InlineKeyboardButton(text="Начать сеанс", callback_data="Start." + str(message.chat.id))
-        start_chating.add(start_sup)
+    #     start_chating = types.InlineKeyboardMarkup()
+    #     start_sup = types.InlineKeyboardButton(text="Начать сеанс", callback_data="Start." + msg_id)
+    #     start_chating.add(start_sup)
 
-        await bot.send_message(sup_id,
-                        "Вам пишет пользователь"
-                        "\nName: " + str(message.chat.first_name) +
-                        "\nid: " + str(message.chat.id)
-                        ,reply_markup=start_chating)
+    #     await bot.send_message(sup_id,
+    #         "Вам пишет пользователь"
+    #         "\nName: " + str(message.chat.first_name) +
+    #         "\nid: " + str(message.chat.id)
+    #         ,reply_markup=start_chating)
 
+    # else:
 
-@dp.callback_query_handler(text_contains='Start.')
-async def ans(call):
-    global client_id, msg_id_client, msg_id_support # !
-    client_id = (call.data.split('.')[1])
-    await bot.send_message(client_id, "Поддержка на связи")
+    #     print("Конец перерыва")
+    #     start_chating = types.InlineKeyboardMarkup()
+    #     start_sup = types.InlineKeyboardButton(text="Начать сеанс", callback_data="Start." + str(message.chat.id))
+    #     start_chating.add(start_sup)
 
-    support_db = Supports_DB()
-    support_db.buzy("true", sup_id)
-
-    over = types.InlineKeyboardMarkup()
-    end_sup = types.InlineKeyboardButton(text="Завершить сеанс", callback_data="end." + str(client_id))
-    over.add(end_sup)
-
-    with open(config['DB']['client'], mode = "r", encoding="utf-8") as fl:
-        quest = json.load(fl)
-
-    await bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
-
-    await bot.send_message(sup_id,
-                        "Начат чат с пользователем"
-                        ,reply_markup=over)
-
-    for i in range(len(quest[client_id]["message"])):
-        msg = await bot.send_message(sup_id, quest[client_id]["message"][i])
-        msg_id_client.append(msg.message_id)
+    #     await bot.send_message(sup_id,
+    #                     "Вам пишет пользователь"
+    #                     "\nName: " + str(message.chat.first_name) +
+    #                     "\nid: " + str(message.chat.id)
+    #                     ,reply_markup=start_chating)
 
 
-@dp.message_handler(content_types='text')
-async def db_add(message: types.Message):
-    global client_id, msg_id_client, msg_id_support
+# @dp.callback_query_handler(text_contains='Start.')
+# async def ans(call):
+#     global client_id, msg_id_client, msg_id_support # !
+#     client_id = (call.data.split('.')[1])
+#     await bot.send_message(client_id, "Поддержка на связи")
 
-    with open(config['DB']['support'], mode = "r", encoding="utf-8") as fl:
-        sup_id_mas = json.load(fl)
+#     support_db = Supports_DB()
+#     support_db.buzy("true", sup_id)
 
-    with open(config['DB']['client'], mode = "r", encoding="utf-8") as fl:
-        quest = json.load(fl)
+#     over = types.InlineKeyboardMarkup()
+#     end_sup = types.InlineKeyboardButton(text="Завершить сеанс", callback_data="end." + str(client_id))
+#     over.add(end_sup)
 
-    if sup_id_mas["telegram_id"]["buzy"] == "true" and str(message.chat.id) != sup_id:
+#     with open(config['DB']['client'], mode = "r", encoding="utf-8") as fl:
+#         quest = json.load(fl)
 
-        msg = await bot.send_message(sup_id, message.text)
-        msg_id_client.append(msg.message_id)
+#     await bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
 
-        client_db = Client_DB()
-        client_id = message.chat.id
-        client_txt = message.text
-        client_db.add(client_id, client_txt)
+#     await bot.send_message(sup_id,
+#                         "Начат чат с пользователем"
+#                         ,reply_markup=over)
 
-    elif str(message.chat.id) == sup_id:
-
-        msg = await bot.send_message(client_id, message.text)
-        msg_id_support.append(message.message_id)
-
-    else:
-
-        client_db = Client_DB()
-        client_id = message.chat.id
-        client_txt = message.text
-        client_db.add(client_id, client_txt)
+#     for i in range(len(quest[client_id]["message"])):
+#         msg = await bot.send_message(sup_id, quest[client_id]["message"][i])
+#         msg_id_client.append(msg.message_id)
 
 
-@dp.callback_query_handler(text_contains='end.')
-async def ans(call):
-    global client_id, msg_id_client, msg_id_support
-    client_id = (call.data.split('.')[1])
+# @dp.message_handler(content_types='text')
+# async def db_add(message: types.Message):
+#     global client_id, msg_id_client, msg_id_support
 
-    support_db = Supports_DB()
-    support_db.buzy("false", sup_id)
+#     with open(config['DB']['support'], mode = "r", encoding="utf-8") as fl:
+#         sup_id_mas = json.load(fl)
 
-    await bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+#     with open(config['DB']['client'], mode = "r", encoding="utf-8") as fl:
+#         quest = json.load(fl)
 
-    for i in range(len(msg_id_client)):
-        await bot.delete_message(chat_id=sup_id, message_id=msg_id_client[i])
+#     if sup_id_mas["telegram_id"]["buzy"] == "true" and str(message.chat.id) != sup_id and str(message.chat.id) == client_id:
+#         print(message.chat.id)
 
-    for i in range(len(msg_id_support)):
-        await bot.delete_message(chat_id=sup_id, message_id=msg_id_support[i])
+#         await bot.send_message(sup_id, message.text)
+#         msg_id_client.append(msg.message_id)
 
-    await bot.send_message(client_id,
-                        "Сеанс завершон")
+#         client_db = Client_DB()
+#         client_id = message.chat.id
+#         client_txt = message.text
+#         client_db.add(client_id, client_txt)
 
-    msg_id_client.clear()
-    msg_id_support.clear()
+#     elif str(message.chat.id) == sup_id:
 
-    client_db = Client_DB()
-    client_db.remove(client_id)
+#         msg = await bot.send_message(client_id, message.text)
+#         msg_id_support.append(message.message_id)
 
-    client_id = ""
+#     else:
+
+#         client_db = Client_DB()
+#         client_id = message.chat.id
+#         client_txt = message.text
+#         client_db.add(client_id, client_txt)
+
+
+# @dp.callback_query_handler(text_contains='end.')
+# async def ans(call):
+#     global client_id, msg_id_client, msg_id_support
+#     client_id = (call.data.split('.')[1])
+
+#     support_db = Supports_DB()
+#     support_db.buzy("false", sup_id)
+
+#     await bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+
+#     for i in range(len(msg_id_client)):
+#         await bot.delete_message(chat_id=sup_id, message_id=msg_id_client[i])
+
+#     for i in range(len(msg_id_support)):
+#         await bot.delete_message(chat_id=sup_id, message_id=msg_id_support[i])
+
+#     await bot.send_message(client_id,
+#                         "Сеанс завершон")
+
+#     msg_id_client.clear()
+#     msg_id_support.clear()
+
+#     client_db = Client_DB()
+#     client_db.remove(client_id)
+
+#     client_id = ""
 
